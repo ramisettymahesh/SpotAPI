3658e28252a8874973c784f0032ea32ef5d93535
ramisettymahesh <ramisetty.venkatanagamaheshwar@cognizant.com>
2025-08-12 11:54:05 +0530

    CORS -update

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
 import SpotifyWebApi from "spotify-web-api-node";
+import cors from 'cors';
+
 
 dotenv.config();
 const app = express();
 const PORT = 3000;
+app.use(cors())
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID || "",
@@ -41,3 +44,4 @@ app.get("/recommend", async (req, res) => {
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
 );
+
